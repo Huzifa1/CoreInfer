18eef302723e694955b1f055983af60f9edafbc6
Eric Schniedermeyer <eric.schniedermeyer@mailbox.org>
2025-04-16 17:05:31 +0200

    add few shot in coreinfer-dataset

--- coreinfer-dataset.py
+++ coreinfer-dataset.py
@@ -53,9 +53,9 @@ def top_p_sampling(next_token_logits, tokenizer, top_p, generated):
 # Test Model
 def generate(method, model, tokenizer, ori_prompt, task_type, num_fewshot, num_tokens_to_generate, device, sampling_method, top_p):
     model.eval()
-    if method in ['stable_guided']:
+    if method in ['stable_guided', 'static_cut', 'dynamic_cut', 'dense', 'moving_cut', 'dynamic_cut_ci']:
         prompt = process_prompt_stable(ori_prompt, task_type, num_fewshot)
-    elif method in ['similarity_guided', 'static_cut', 'dynamic_cut', 'dense', 'moving_cut', 'dynamic_cut_ci']:
+    elif method in ['similarity_guided']:
         prompt = process_prompt_similarity(ori_prompt, task_type)
     input_ids = tokenizer.encode(prompt, return_tensors='pt').to(device)
 
