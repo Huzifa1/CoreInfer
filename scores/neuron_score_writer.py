4cbc8a64150d283e743604a7e142047a0e67cfa4
Eric Schniedermeyer <eric.schniedermeyer@mailbox.org>
2025-05-13 22:17:08 +0200

    no double naming of task name in score filename

--- scores/neuron_score_writer.py
+++ scores/neuron_score_writer.py
@@ -14,7 +14,7 @@ def write_neuron_scores(scores: Tensor, dataset_output_path: str, command_str: s
         file_str += "\n"
     
         file_type = dataset_output_path.split(".")[-1]
-        output_path = dataset_output_path.replace("_score", "").replace("dataset_run", "scores").replace(f".{file_type}", f"_{task_name}_{round(sparsity_level, 2)}.score")
+        output_path = dataset_output_path.replace("_score", "").replace("dataset_run", "scores").replace(f".{file_type}", f"_{round(sparsity_level, 2)}.score")
         with open(output_path, "a") as output_file:
             output_file.write(file_str)
         file_str = ""
